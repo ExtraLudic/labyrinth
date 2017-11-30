6719558bda211e6f2c734cc0b53167cbb4b1de89
barrettand <barrett.and@husky.neu.edu>
2017-11-30 13:40:32 -0500

    New token

--- Daedalus.js
+++ Daedalus.js
@@ -22,7 +22,7 @@ var labyrinthchannelid = 'C7TBY98TS';
 var daedalusemoji = "https://avatars.slack-edge.com/2017-11-08/269162770516_e2c4553016a99b14da83_72.png";
 var bot = controller.spawn({
 
-  token: "xoxb-270188256679-P8MGCpVJi5qwfniKPKY2Czus"
+  token: "xoxb-270188256679-NbLhI1dDJq2KoEnsBVE0pfBN"
 
 })
 
