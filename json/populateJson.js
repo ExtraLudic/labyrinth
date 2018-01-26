0c3a88b34bc61346b31f49445eef9e3fd57fe803
wade <richardkimbrough@Richards-MBP.cable.rcn.com>
2018-01-26 17:03:11 -0500

    Updated JSON files and populateJson scipt. It now adds a # to incorrect

--- json/populateJson.js
+++ json/populateJson.js
@@ -11,63 +11,95 @@ puzzle.name = 'puzzle';
 var description = [];	
 description.name = 'description';					// ENABLE WHEN YOU HAVE ROOM DESCRIPTIONS
 
-/*
 
-fs.open('myfile', 'r', (err, fd) => {
-  if (err) {
-    if (err.code === 'ENOENT') {
-      console.error('myfile does not exist');
-      return;
-    }
- 
-    throw err;
-  }
- 
-  readMyData(fd);
-});
 
-*/
 
-function populateArray(arr, size){
-	for(var i = 0; i < size; i++){
-	    let directory = "./" + arr.name;
-	    let fileName = "/" + arr.name + "_" + i + ".txt";
-		//let populateArr =
-		fs.openSync(directory + fileName, (err, fd) =>{
-			if (err){
-				if(err.code === 'ENOENT' ){
-					console.error('myfile does not exist');
-					return;
-				}
-				throw err;;
-			}
-			readMyData(fd, 'utf-8');
-		});
-		
+
+//STEP 1: POPULATE ARRAYS WITH ROOM DESCRIPTIONS, KEYS, AND PUZZLES
+
+function populateArray(arr){
+	//console.log(arr.name);
+	//var newArr = [];
+	var listDir = fs.readdirSync('./'+ arr.name);
+	var sortDir = listDir.sort((a, b) =>{  
+		 a=a.split("_");
+   		 b=b.split("_");
+   		 //console.log(a);
+   		 return parseInt(a[1]) - parseInt(b[1]);
+	});
+	//console.log(listDir);
+	console.log(sortDir);
+	for(var i = 0; i < sortDir.length; i++){
+
+		var populateArr = fs.readFileSync('./'+ arr.name + "/" + sortDir[i] , 'utf-8');
+		arr.push(populateArr);
+		//console.log(newArr);
 	}
-	
+	//return newArr;
 }
+	
+populateArray(key);
+populateArray(puzzle);
+populateArray(description);
 
-var lto = function listToObject(list){
+//console.log(description);
+
+
+//END OF STEP ONE. ALL ARRAYS ARE NOW POPULATED
+
+//STEP TWO RANDOMIZE THE KEYS SO THAT THE CORRECT ANSWER IS NOW ALWAYS FIRST
+
+/*
+var lto = function listToObject(list, index){
+let value = "incorrect" + index;
 let objectFormat = {
             "text": undefined,
-            "value": 'incorrect'
+            "value": value
              };
 
 objectFormat.text = list;
 return objectFormat;
 }
+*/
 
-function randomizeKeys(key){
+var rk = function randomizeKeys(key){
 	let list = key.split(",");
-	let formattedList = list.map(lto);
+	let formattedList = list.map((currentElement, index) => {
+		let value = "incorrect" + index;
+		let objectFormat = {
+            "text": undefined,
+            "value": value
+             };
+
+		objectFormat.text = list;
+		return objectFormat;
+	});
 	formattedList[0].value = 'correct';
 	let randomKey = formattedList.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
-	key = key.push(randomKey);
+	//key = key.push(randomKey);
+	return randomKey;
 }
 
+var randomKeys = key.map(rk);
 
-function writeInPuzzlesKeys(studioScriptParsed){
+console.log(randomKeys);
+
+// END OF STEP TWO. KEYS ARE NOW RANDOM.
+
+// STEP THREE. TAKE THE DESCRIPTIONS ARRAY AND SPLIT IT INTO TITLES AND DESCRIPTIONS
+
+var sd = function splitDescriptions(description){
+	let split = description.split("~");
+	return split;
+}
+
+var titleDescription = description.map(sd);
+
+//console.log(titleDescription[0]);
+
+
+
+function addContent(studioScriptParsed){
 
 	for(var i = 0; i < studioScriptParsed.length; i++){
 		//console.log(studioScriptParsed[i].script);
@@ -78,16 +110,24 @@ function writeInPuzzlesKeys(studioScriptParsed){
 			for(var v = 0; v < studioScriptParsed[i].script[j].script.length; v++){
 				//console.log(studioScriptParsed[i].script[j].script[v].attachments[0]);
 				if (studioScriptParsed[i].script[j].script[v].attachments){
-					for(var s = 1; s < 32; s++){
+					for(var s = 1; s < 33; s++){
+						var arrayIndex = s-1;
+						var titleDescriptionArr = titleDescription[arrayIndex];
+						
 						//console.log('s'+s);
-					    // ADD CODE ABOUT ROOM DESCRIPTIONS HERE. ATTACHMENTS[0].text.                    //UPDATE WHEN YOU HAVE ROOM DESCRIPTIONS
+					    // ADD CODE ABOUT ROOM DESCRIPTIONS HERE. ATTACHMENTS[0].text.
+					    if (studioScriptParsed[i].script[j].script[v].attachments[0].callback_id == "room_"+s){
+					   // console.log("title................", titleDescriptionArr[0]);
+					    //studioScriptParsed[i].script[j].script[v].attachments[0].title = titleDescriptionArr[0]; 
+					    studioScriptParsed[i].script[j].script[v].attachments[0].text  = titleDescriptionArr[1];                   
+					    }
 
 						if (studioScriptParsed[i].script[j].script[v].attachments[0].callback_id == "key_"+s){
 							//console.log('xxxxxxxxxxxxxxxxxxxx');
 
-							var arrayIndex = s-1;
-					        studioScriptParsed[i].script[j].script[v].attachments[0].actions[0].options = keys[arrayIndex];
-					        studioScriptParsed[i].script[j].script[v].attachments[0].text = puzzles[arrayIndex];
+							
+					        studioScriptParsed[i].script[j].script[v].attachments[0].actions[0].options = randomKeys[arrayIndex];
+					        studioScriptParsed[i].script[j].script[v].attachments[0].text = puzzle[arrayIndex];
 				        }
 					}
 				}	
@@ -101,88 +141,21 @@ function writeInPuzzlesKeys(studioScriptParsed){
 }
 
 
-var keys = populateArray(key, 32);
+//var keys = populateArray(key, 32);
 
-console.log(keys);
+//console.log(keys);
 
-/*
-populateKPRarrays();
 
-//console.log(room_jsons);
-var labyrinthJson = writeInPuzzlesKeys(room_jsons);
+
 
 //console.log(room_jsons);
+var labyrinthJson = addContent(studioScriptParsed);
+
+console.log('writing...');
 fs.writeFileSync('./labyrinth/labyrinth.json', JSON.stringify(labyrinthJson), 'utf8');
-*/
 
 
-/*
-function populateKPRarrays(){
-	for (var i = 2; i < 32; i++){
-	// Bring in JSON Files and turn them into Objects	
 
-	// Bring in keys. Seperate by , . Make first value correct. Then make random.
-	let key = fs.readFileSync("./keys/key_" + i + ".txt", "utf-8");
-	let list = key.split(",");
-	let formattedList = list.map(lto);
-	formattedList[0].value = 'correct';
-	let randomKey = formattedList.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
-	//Bring in puzzles.
-	let puzzle = fs.readFileSync("./puzzles/puzzle_" + i + ".txt", "utf-8");
-	//let descriptions = fs.readFileSync("./descriptions/description_" + i + ".txt", "utf-8");      // ENABLE WHEN YOU HAVE ROOM DESCRIPTIONS
-	// add all to respective arrays
-	//roomDescriptions.push(descriptions);															// ENABLE WHEN YOU HAVE RROM DESCIPTIONS
-	keys.push(randomKey);
-	puzzles.push(puzzle);
-	//room_jsons.push(studioScript);
-	}
-}
-
-
-var lto = function listToObject(list){
-let objectFormat = {
-            "text": undefined,
-            "value": 'incorrect'
-             };
-
-objectFormat.text = list;
-return objectFormat;
-}	
-
-function writeInPuzzlesKeys(room_jsons){
-
-	for(var i = 0; i < room_jsons.length; i++){
-		//console.log(room_jsons[i].script);
-
-		for(var j = 0; j < room_jsons[i].script.length; j++){
-			//console.log(i, room_jsons[i].script.length);
-		//	console.log(room_jsons[i].script[j].script.length);
-			for(var v = 0; v < room_jsons[i].script[j].script.length; v++){
-				//console.log(room_jsons[i].script[j].script[v].attachments[0]);
-				if (room_jsons[i].script[j].script[v].attachments){
-					for(var s = 1; s < 32; s++){
-						//console.log('s'+s);
-					    // ADD CODE ABOUT ROOM DESCRIPTIONS HERE. ATTACHMENTS[0].text.                    //UPDATE WHEN YOU HAVE ROOM DESCRIPTIONS
-
-						if (room_jsons[i].script[j].script[v].attachments[0].callback_id == "key_"+s){
-							//console.log('xxxxxxxxxxxxxxxxxxxx');
-
-							var arrayIndex = s-1;
-					        room_jsons[i].script[j].script[v].attachments[0].actions[0].options = keys[arrayIndex];
-					        room_jsons[i].script[j].script[v].attachments[0].text = puzzles[arrayIndex];
-				        }
-					}
-				}	
-			}
-		}
-
-	}
-
-	return room_jsons;
-
-}
-
-*/
 
 
 
